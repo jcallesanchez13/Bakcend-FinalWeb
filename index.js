0446804949aa2c8584aeaf4e9c353e0abce7371c
jcallesanchez13 <callesanchezjulian@gmail.com>
2025-06-09 17:59:30 -0500

    actualizacion

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/usuarios', rutasUsuarios);
 app.use('/categorias', rutasCategorias);
 app.use('/transacciones', rutasTransacciones);
 app.use('/analisis', rutasAnalisis);
-
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Servidor backend de Finanzas Personales funcionando correctamente.');
